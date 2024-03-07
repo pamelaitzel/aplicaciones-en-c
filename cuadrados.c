4943f9163f9f475af1d400100a7e48bfb33a1d0a
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-03-06 21:11:56 -0600

    Create cuadrados.c

---
+++ cuadrados.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 500) {
+        int cuadrado = i * i;
+        int cubo = i * i * i;
+        printf("Número: %d, Cuadrado: %d, Cubo: %d\n", i, cuadrado, cubo);
+        i++;
+    }
+
+    return 0;
+}
