d10006fa1ba496c98c24daacbbacca202e061803
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-03-13 15:23:33 -0600

    Create hilo.c

---
+++ hilo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *funcionHilo(void *arg) {
+    printf("¡Hola desde el hilo!\n");
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t hilo;
+    pthread_create(&hilo, NULL, funcionHilo, NULL);
+    pthread_join(hilo, NULL);
+    return 0;
+}
