e67897920141ef8830cc7fc13fda679ab2fc3f48
pamelaitzel <135767172+pamelaitzel@users.noreply.github.com>
2024-04-04 09:46:41 -0600

    usuario

--- name.c
+++ name.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 int main() {
+
     char hostname[1024];
+
     gethostname(hostname, sizeof(hostname));
+    
     printf("Hostname: %s\n", hostname);
     return 0;
 }
